b814e198fd4c3ff4ed6858d8780b1905be7a9a6a
Hayden Galyean <haydengalyeanbiz@gmail.com>
2025-07-05 16:49:53 -0400

    working test submit slash command

---
+++ dune_ledger_bot/src/commands/submit.rs
@@ -0,0 +1,13 @@
+
+use crate::Context;
+use crate::Error;
+
+#[poise::command(slash_command)]
+pub async fn submit(
+    ctx: Context<'_>,
+    #[description = "Resource to submit"] resource: String,
+    #[description = "Amount to submit"] amount: i32,
+) -> Result<(), Error> {
+    ctx.say(format!("Pong! you submitted {} of {}", amount, resource)).await?;
+    Ok(())
+}
