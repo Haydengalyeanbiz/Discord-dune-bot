1063be91cdfbe89764d1252dce5d122d46e36609
Hayden Galyean <haydengalyeanbiz@gmail.com>
2025-07-07 16:52:03 -0400

    google connecting successfully

--- dune_ledger_bot/src/commands/request.rs
+++ dune_ledger_bot/src/commands/request.rs
@@ -1,11 +1,8 @@
-use crate::{Context, Error};
+use crate::{Context, BotError};
+// use regex::Regex;
 
-#[poise::command(
-    slash_command,
-    subcommands("start"/*,"bulk-add"*/), // TODO: bulk add functionality
-    subcommand_required
-)]
-pub async fn request(_: Context<'_>) -> Result<(), Error> {
+#[poise::command(slash_command, subcommands("start", "bulk_add"), subcommand_required)]
+pub async fn request(_: Context<'_>) -> Result<(), BotError> {
     Ok(())
 }
 
@@ -13,7 +10,27 @@ pub async fn request(_: Context<'_>) -> Result<(), Error> {
 pub async fn start(
     ctx: Context<'_>,
     #[description = "Title for the request"] product: String,
-) -> Result<(), Error> {
-    ctx.reply(format!("Request recieved for: {}.", product)).await?;
+) -> Result<(), BotError> {
+    ctx.reply(format!("Request recieved for: {}.", product))
+        .await?;
+    Ok(())
+}
+
+async fn sanitize(ctx: Context<'_>, input: &str) -> Result<(), BotError> {
+    let sanitized_list = input.replace(',', "").replace(" x ", " ").replace("-", "");
+    let output: String = format!(
+        "✅ Sanitized the following resources:\n```\n{}\n```",
+        sanitized_list,
+    );
+    ctx.say(output).await?;
+    Ok(())
+}
+
+#[poise::command(slash_command)]
+pub async fn bulk_add(
+    ctx: Context<'_>,
+    #[description = "Paste the raw resouce list here"] raw_resource_list: String,
+) -> Result<(), BotError> {
+    sanitize(ctx, &raw_resource_list).await?;
     Ok(())
 }
